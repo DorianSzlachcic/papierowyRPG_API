558fad02252869526c41e49cc2e3a19bcb13ccaf
DorianSzlachcic <dorianszlachcic@gmail.com>
2024-10-26 20:22:51 +0200

    Add simple users handling

---
+++ papierowyRPG_API/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using papierowyRPG_API.Models;
+using papierowyRPG_API.Services;
+using System.ComponentModel;
+
+namespace papierowyRPG_API.Controllers
+{
+    [Route("api/users")]
+    [ApiController]
+    public class UserController
+    {
+        private readonly UserService userService;
+
+        public UserController()
+        {
+            userService = new UserService();
+        }
+
+        [HttpGet]
+        [ProducesResponseType<User>(StatusCodes.Status200OK)]
+        public List<User> Get()
+        {
+            return userService.GetUsers();
+        }
+    }
+}
