48dba5a1fd8a8f8cfd707c0dfc0efbbe8dc96999
DorianSzlachcic <92079129+DorianSzlachcic@users.noreply.github.com>
2024-10-26 20:34:44 +0200

    Add simple users handling (#1)
    * Add simple users handling
    
    * .gitignore
    
    * ignore
    
    * ignore

---
+++ papierowyRPG_API/Database/UserContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using papierowyRPG_API.Models;
+
+namespace papierowyRPG_API.Database
+{
+    public class UserContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public string DbPath;
+
+        public UserContext()
+        {
+            DbPath = "sqlite.db";
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseSqlite($"Data Source={DbPath}");
+    }
+}
---
+++ papierowyRPG_API/Services/UserService.cs
@@ -0,0 +1,20 @@
+﻿using papierowyRPG_API.Database;
+using papierowyRPG_API.Models;
+
+namespace papierowyRPG_API.Services
+{
+    public class UserService
+    {
+        private UserContext userContext;
+
+        public UserService()
+        {
+            userContext = new UserContext();
+        }
+
+        public List<User> GetUsers()
+        {
+            return userContext.Users.ToList();
+        }
+    }
+}
