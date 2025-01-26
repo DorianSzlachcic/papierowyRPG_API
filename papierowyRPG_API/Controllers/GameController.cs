a3482eb77a7d9bdebb8a8068f63f7e50a1bb9301
Jakub Szklarz <kubusisko1@gmail.com>
2025-01-26 20:58:26 +0100

    quick fix
    form acceptance fixed

---
+++ papierowyRPG_API/Controllers/GameController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using papierowyRPG_API.Models;
+using papierowyRPG_API.Services;
+using papierowyRPG_API.Forms;
+
+namespace papierowyRPG_API.Controllers
+{
+    [Route("api/games")]
+    [ApiController]
+    public class GameController(IGameService gameService) : ControllerBase
+    {
+        [HttpGet]
+        [ProducesResponseType<List<GameDTO>>(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult Get()
+        {
+            var hold = gameService.GetGames();
+            return  hold == null ? BadRequest() : Ok(hold);
+        }
+
+        [HttpPost("add")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult Add([FromForm] GameForm gameForm)
+        {
+            var test = gameService.CreateGame(gameForm.name, gameForm.ruleset, gameForm.gameMaster, gameForm.player1, gameForm.player2, gameForm.player3, gameForm.player4);
+            return test == null ? BadRequest() : Ok(test);
+        }
+
+        [HttpGet("games")]
+        [ProducesResponseType<List<GameDTO>>(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult Games(string name)
+        {
+            var test = gameService.GetGamesForPlayer(name);
+            return test == null ? BadRequest() : Ok(test);
+        }
+    }
+}
---
+++ papierowyRPG_API/Forms/GameForms.cs
@@ -0,0 +1,13 @@
+﻿namespace papierowyRPG_API.Forms
+{
+    public class GameForm
+    {
+        public required string name {  get; set; }
+        public required string ruleset { get; set; }
+        public required string player1 { get; set; }
+        public required string player2 { get; set; }
+        public required string player3 { get; set; }
+        public required string player4 { get; set; }
+        public required string gameMaster { get; set; }
+    }
+}
